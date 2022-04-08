13772e0dc0b820f34f03e1258999013529cb25f2
LefterStud <101552997+LefterStud@users.noreply.github.com>
2022-04-08 19:50:07 +0300

    Repository created

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+/**
+ * Lefter Olexii, #18
+ */
+public class Main {
+
+    /**
+     * Дано целое положительное число. Вывести символы,
+     * изображающие цифры этого числа (в порядке слева направо).
+     */
+    private static void task20(){
+        System.out.println("\n\nTask 20");
+
+    }
+
+    /**
+     * Дана строка, состоящая из слов кириллицей,
+     * набранных заглавными буквами и разделенных пробелами (одним или несколькими).
+     * Найти количество слов, которые содержат хотя бы одну букву «А».
+     */
+    private static void task43(){
+        System.out.println("\n\nTask 43");
+    }
+
+    /**
+     * Дано четное число N (> 0) и символы C1 и C2. Вывести строку длины N,
+     * которая состоит из чередующихся символов C1 и C2, начиная с C1.
+     */
+    private static void task9(){
+        System.out.println("Task 9");
+    }
+
+    public static void main(String[] args) {
+	task20();
+    task43();
+    task9();
+    }
+}
