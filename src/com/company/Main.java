f249c58ad60c4965a06f423fd62ba1be16c67c21
LefterStud <101552997+LefterStud@users.noreply.github.com>
2022-04-08 21:30:37 +0300

    task 9 completed, task 43 started

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,8 +9,8 @@ public class Main {
      * Дано целое положительное число. Вывести символы,
      * изображающие цифры этого числа (в порядке слева направо).
      */
-    private static void task20(){
-        System.out.println("\n\nTask 20");
+    private static void task20() {
+        System.out.println("Task 20");
 
     }
 
@@ -19,21 +19,34 @@ public class Main {
      * набранных заглавными буквами и разделенных пробелами (одним или несколькими).
      * Найти количество слов, которые содержат хотя бы одну букву «А».
      */
-    private static void task43(){
+    private static void task43() {
         System.out.println("\n\nTask 43");
+        int spaceCounter=0;
+        String wordString = "ПОЕЗД КОМПЬЮТЕР МАШИНА СЛОВО ТАНК";
+        System.out.print(wordString.charAt(2));
     }
 
     /**
      * Дано четное число N (> 0) и символы C1 и C2. Вывести строку длины N,
      * которая состоит из чередующихся символов C1 и C2, начиная с C1.
      */
-    private static void task9(){
-        System.out.println("Task 9");
+    private static void task9() {
+        System.out.println("\n\nTask 9");
+        final int N = 100;
+        final char c1 = 'A';
+        final char c2 = 'B';
+        for (int i = 1; i <= N; i++) {
+            if ((i % 2) != 0) {
+                System.out.print(c1);
+            } else {
+                System.out.print(c2);
+            }
+        }
     }
 
     public static void main(String[] args) {
-	task20();
-    task43();
-    task9();
+        task20();
+        task43();
+    //    task9();
     }
 }
